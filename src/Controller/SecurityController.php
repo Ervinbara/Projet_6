d2468ad4dd3c0ab6a27c73aa17fc1c4010a4acce
Your name here <ebara@fft.fr>
2021-12-18 15:16:24 +0100

    Update password with email send

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,14 +5,19 @@ namespace App\Controller;
 use App\Entity\User;
 use Twig\Environment;
 use App\Form\RegistrationType;
+use App\Form\ResetPasswordType;
 use Symfony\Component\Mime\Email;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 
 class SecurityController extends AbstractController
 {
@@ -109,5 +114,121 @@ class SecurityController extends AbstractController
     {
         // controller can be blank: it will never be called!
         throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
+
+    /**
+     * @Route("/forgot_password", name="security_forgot_password")
+     */
+    public function forgotPass(
+        Request $request, 
+        UserRepository $user, 
+        MailerInterface $mailer, 
+        Environment $twig, 
+        TokenGeneratorInterface $tokenGenerator): Response
+    {
+        // On initialise le formulaire
+        $form = $this->createForm(ResetPasswordType::class);
+
+        // On traite le formulaire
+        $form->handleRequest($request);
+
+        // Si le formulaire est valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            // On récupère les données
+            $donnees = $form->getData();
+
+            // On cherche un utilisateur ayant cet e-mail
+            $user = $user->findOneByEmail($donnees['email']);
+
+            // Si l'utilisateur n'existe pas
+            if ($user === null) {
+                // On envoie une alerte disant que l'adresse e-mail est inconnue
+                $this->addFlash('warning', 'Cette adresse e-mail est inconnue');
+                
+                // On retourne sur la page de connexion
+                return $this->redirectToRoute('security_forgot_password');
+            }
+
+            // On génère un token
+            $token = $tokenGenerator->generateToken();
+            // $user->setTokenActivation(md5(uniqid()));
+
+            // On essaie d'écrire le token en base de données
+            try{
+                $user->setTokenActivation($token);
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
+            } catch (\Exception $e) {
+                $this->addFlash('warning', $e->getMessage());
+                return $this->redirectToRoute('security_login');
+            }
+
+            // On génère l'URL de réinitialisation de mot de passe
+            $url = $this->generateUrl('security_reset_password', array('token' => $token), UrlGeneratorInterface::ABSOLUTE_URL);
+
+            // On génère l'e-mail
+            $email = (new Email())
+                ->to('ervinbara17@gmail.com')
+                ->subject('Time for Symfony Mailer!') 
+                ->text('Bonjour, Une demande de réinitialisation de mot de passe a été effectuée. Veuillez cliquer sur le lien suivant : ' . $url);
+                // ->html($twig->render('security/reset_password.html.twig', ['token' => $user->getTokenActivation()] ));
+            $mailer->send($email);
+
+            // On crée le message flash de confirmation
+            $this->addFlash('success', 'E-mail de réinitialisation du mot de passe envoyé !');
+
+            // On redirige vers la page de login
+            return $this->redirectToRoute('security_login');
+        }
+
+    // On envoie le formulaire à la vue
+    return $this->render('security/forgot_password.html.twig',['emailForm' => $form->createView()]);
+    }
+
+
+    /**
+     * @Route("/reset_password/{token}", name="security_reset_password")
+     */
+    public function resetPassword(
+        Request $request,
+        string $token,
+        UserRepository $user,
+        UserPasswordHasherInterface $encoder,
+        EntityManagerInterface $manager)
+    {
+        // On cherche un utilisateur avec le token donné
+        $user = $user->findOneBy(['token_activation' => $token]);
+
+        // Si l'utilisateur n'existe pas
+        if ($user === null) {
+            // On affiche une erreur
+            $this->addFlash('warning', 'Token Inconnu');
+            return $this->redirectToRoute('security_login');
+        }
+
+        // Si le formulaire est envoyé en méthode post
+        if ($request->isMethod('POST')) {
+            // On supprime le token
+            $user->setTokenActivation(null);
+
+            // On chiffre le mot de passe
+            // $hash = $encoder->hashPassword($user, $user->getPassword());
+            $user->setPassword($encoder->hashPassword($user, $request->request->get('password')));
+
+            // On stocke en base 
+            $manager->persist($user);
+            $manager->flush();
+
+            // On crée le message flash
+            $this->addFlash('success', 'Mot de passe mis à jour !');
+
+            // On redirige vers la page de connexion
+            return $this->redirectToRoute('security_login');
+        }else {
+            // Si on n'a pas reçu les données, on affiche le formulaire
+            return $this->render('security/reset_password.html.twig', ['token' => $token]);
+        }
+
     }
 }
